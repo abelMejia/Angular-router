c61fce349647f85f8ba65a3a6120d98ea724b570
Abelm <abel.mejia@adesis.com>
2016-03-18 18:15:30 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+
+app.use(express.static('public/')) 
+
+app.use('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname, 'public/'))
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
